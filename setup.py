63df44c67f202d9a6c77a80bfede9d3926fe8944
Wambugu71 <wambugukinyua125@gmail.com>
2024-08-23 23:24:58 +0300

    push package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='streamlit_push_notifications',
+  version='0.1',
+  packages=find_packages(),
+  description='A Streamlit library for sending browser notifications and alerts',
+  author='Wambugu Kinyua',
+  author_email='kenliz1738@gmail.com',
+  url='https://github.com/yunisguliyev/streamlit-push-notifications.git', 
+  classifiers=[
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+  ],
+  install_requires=[
+      'streamlit>=1.0.0', 
+  ],
+  python_requires='>=3.8',
+)
